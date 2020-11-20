3e86df34f75d3b572b0ef5c3c77870d030305704
ScarKMS <47459571+ScarKMS@users.noreply.github.com>
2020-11-20 00:08:38 -0600

    Salto Implementado
    - Se combino el Script playerMov con el Scrpit de Salto
    -Se implemento la animación de salto
    -Salto completamente implementado

--- Assets/Scripts/Player/playerMov.cs
+++ Assets/Scripts/Player/playerMov.cs
@@ -4,20 +4,65 @@ using UnityEngine;
 
 public class playerMov : MonoBehaviour
 {
-   void Update()
+
+    void Start(){
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    void Update()
     {
         Mover();
+        salto();
+        if(comprobandoSalto) compararPosicion();
     }
 
-    /**Aspecto generales del Jugador**/
-    public float velocidad = 7f;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer sr;
+    private Animator animator;
+    bool puedeSaltar = true, comprobandoSalto = false;
+    public float fuerzaSalto;
+    public float posInicialSalto;
+    float velocidad = 5f;
 
-    /**Componentes del Jugador**/
     private void Mover() {
+
+        
+        
         float posX = velocidad * Time.deltaTime * Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(posX, 0, 0);
+        if( posX != 0 ) sr.flipX = posX < 0;
+        if(puedeSaltar){
+            animator.SetBool("IsRunning",
+            Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.D));
+            float posY = velocidad * Time.deltaTime * Input.GetAxisRaw("Vertical");
+            transform.position += new Vector3(0, posY, 0);
+        }
+    }
 
-        float posY = velocidad * Time.deltaTime * Input.GetAxisRaw("Vertical");
-        transform.position += new Vector3(0, posY, 0);
+    void salto(){
+        bool saltando = Input.GetKey(KeyCode.Space);
+        if(saltando && puedeSaltar){
+            velocidad = 2f;
+            posInicialSalto = transform.position.y;
+            rb2d.gravityScale = 1;
+            rb2d.AddForce(new Vector2(0, fuerzaSalto));
+            puedeSaltar = false;
+            comprobandoSalto = true;
+            animator.SetBool("IsJumping", true);
+        }
+    }
+
+    void compararPosicion(){
+        if(posInicialSalto > transform.position.y){
+            rb2d.constraints = RigidbodyConstraints2D.FreezePositionY;
+            rb2d.gravityScale = 0;
+            puedeSaltar = true;
+            comprobandoSalto = false;
+            velocidad = 5f;
+            animator.SetBool("IsJumping", false);
+            rb2d.constraints = RigidbodyConstraints2D.None;
+            rb2d.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
     }
 }
