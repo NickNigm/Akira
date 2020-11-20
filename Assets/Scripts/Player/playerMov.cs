9e3a9f4dff0ab2124d97f4c5768887e051187e08
ScarKMS <47459571+ScarKMS@users.noreply.github.com>
2020-11-19 19:02:19 -0600

    Moviento
    Movimiento del jugador nuevo

---
+++ Assets/Scripts/Player/playerMov.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMov : MonoBehaviour
+{
+   void Update()
+    {
+        Mover();
+    }
+
+    /**Aspecto generales del Jugador**/
+    public float velocidad = 7f;
+
+    /**Componentes del Jugador**/
+    private void Mover() {
+        float posX = velocidad * Time.deltaTime * Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(posX, 0, 0);
+
+        float posY = velocidad * Time.deltaTime * Input.GetAxisRaw("Vertical");
+        transform.position += new Vector3(0, posY, 0);
+    }
+}
