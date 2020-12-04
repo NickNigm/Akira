82583ecdcb0c40ec19fcc9d054647de9097557a7
ScarKMS <47459571+ScarKMS@users.noreply.github.com>
2020-12-03 23:20:29 -0600

    Expulsion
    Se agrego con Q la expulsion, beta falta vfx

---
+++ Assets/Scripts/Player/Poderes/Expansion.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expansion : MonoBehaviour
+{
+
+    public playerController sPC;
+
+    public float velocidad = 50f;
+    public float incremento = 2.5f;
+    public float decremento = 0.2f;
+    public float costoEnergia = 100f;
+    public float minimoEnergia = 10f;
+
+    public bool expandiendose = false;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        comprobarExpancion();
+    }
+
+    void comprobarExpancion(){
+        expandiendose = Input.GetKey(KeyCode.Q);
+        if(expandiendose && sPC.energia>=minimoEnergia) expandir();
+        else comprimir();
+    }
+
+    void expandir(){
+        float x = transform.localScale.x, y = transform.localScale.y, z = transform.localScale.z;
+        if(transform.localScale.x < incremento && transform.localScale.y < incremento && transform.localScale.z < incremento)
+        transform.localScale = new Vector3(x+Time.deltaTime*velocidad, y+Time.deltaTime*velocidad, z+Time.deltaTime*velocidad);
+        sPC.cambioEnergia(costoEnergia * Time.deltaTime, true);
+    }
+
+    void comprimir(){
+        float x = transform.localScale.x, y = transform.localScale.y, z = transform.localScale.z;
+        if(transform.localScale.x > decremento && transform.localScale.y > decremento && transform.localScale.z > decremento)
+        transform.localScale = new Vector3(x-Time.deltaTime*velocidad, y-Time.deltaTime*velocidad, z-Time.deltaTime*velocidad);
+    }
+    
+}
--- Assets/Scripts/Player/playerController.cs
+++ Assets/Scripts/Player/playerController.cs
@@ -8,18 +8,18 @@ public class playerController : MonoBehaviour
 
     public Text txtEnergia;
     public Text txtVida;
-    public int energia;
-    public int vida;
+    public float energia;
+    public float vida;
 
-    public void cambioEnergia(int energiaModificar, bool ocupo){
+    public void cambioEnergia(float energiaModificar, bool ocupo){
         if(ocupo) energia -= energiaModificar;
         else energia += energiaModificar;
-        txtEnergia.text = "Energia: " +energia+ "%";
+        txtEnergia.text = "Energia: " +energia.ToString("f2")+ "%";
     }
 
-    public void cambioVida(int vidaModificar, bool daño){
+    public void cambioVida(float vidaModificar, bool daño){
         if(daño) vida -= vidaModificar;
         else vida += vida;
-        txtVida.text = "Vida: " +vida+ "%";
+        txtVida.text = "Vida: " +vida.ToString("f2")+ "%";
     }
 }
